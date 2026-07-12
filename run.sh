30716c13ccd186a99dc2625d99e6f71ab6d8cd16
shao <shaoyu.ts@gmail.com>
2026-07-12 17:09:46 +0800

    change fB03 into urban case

--- run.sh
+++ run.sh
@@ -95,8 +95,8 @@ python ${PYROTATE} ./fig/pbl_initial.pdf ${OUTDIR}/fA02.pdf
 python ${PYROTATE} ./fig/tg2_VVMex_urban.pdf ${OUTDIR}/f07ab.pdf
 python ${PYROTATE} ./fig/tg2_VVMex_grass.pdf ${OUTDIR}/f07cd.pdf
 python ${PYROTATE} ./fig/tg2_VVMex_evergreen.pdf ${OUTDIR}/fB04.pdf
-python ${PYROTATE} ./fig/tg_VVMex_grass.pdf ${OUTDIR}/fB03a.pdf
-python ${PYROTATE} ./fig/tg_VVM_grass.pdf ${OUTDIR}/fB03b.pdf
+python ${PYROTATE} ./fig/tg_VVMex_urban.pdf ${OUTDIR}/fB03a.pdf
+python ${PYROTATE} ./fig/tg_VVM_urban.pdf ${OUTDIR}/fB03b.pdf
 cd ${SRCDIR}
 
 # fig 8, rcemip
